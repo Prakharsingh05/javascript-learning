70a1a4494655534120e515d5ab13c1967f5d0ec4
Prakhar Singh <prakharsingh285@gmail.com>
2023-04-30 01:44:26 +0530

    updated

---
+++ datatypes and operators/ternaryoperator.js
@@ -0,0 +1,22 @@
+// ternay operator
+
+let age= 15;
+let drink ;
+
+if(age>5){
+    drink = "coffee";
+
+}else{
+    drink = "milk";
+}
+
+// the above written code is right but length this whole code can be compressed into a single line of cide using ternary operator
+// ternary operator is  also +called condional operator
+
+let age1 = 8;
+
+let drink1 = age >=5 ? "coffee" : "milk"; // if true than coffee will be considered else milk
+
+
+
+
