5d95d25c75f0405eaffc1b98594dd13dbd7006c5
Prakhar Singh <prakharsingh285@gmail.com>
2023-04-29 22:25:46 +0530

    updated

--- datatypes/variables.js
+++ datatypes/variables.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 //intro to variables 
 
 //variables can store some information 
@@ -7,43 +7,62 @@
 
 // declaring a string variable
 
-var firstName =  "Prakhar singh";
+//var firstName =  "Prakhar singh";
 
 //printing the variable using variable name
-document.write(firstName);
+//document.write(firstName);
 
 // changing the variable
 
-firstName = "Prakhyat  singhal";
+//firstName = "Prakhyat  singhal";
 
-document.write(firstName);
+//document.write(firstName);
 
 //both the name will print one time
 
 
 //let keywords 
 
-let secondName = "Aishwarya soni";
-document.write(secondName);
+//let secondName = "Aishwarya soni";
+//document.write(secondName);
 
 // block scope vs function scope make difference in var vs let 
 
 
 //declare constants
 
-const pi = 3.14;
+//const pi = 3.14;
 
 //
  //string indexing 
-  let firstName = "Prakhar singh";
+  //let firstName = "Prakhar singh";
 
-  document.write(firstName[0]); // will print P
-  document.write(firstName[0]); // will print r
-  document.write(firstName[0]); // will print a
+  //document.write(firstName[0]); // will print P
+  //document.write(firstName[0]); // will print r
+  //document.write(firstName[0]); // will print a
 
   // last index = length -1;
 
 
   // to find the length
 
-  document.write(firstName.length);
+  //document.write(firstName.length);
+
+  // strings method
+
+  let nameName = "     Prakhar singh    ";
+
+
+  console.log(nameName.length);
+    
+  // to remove spaces
+
+  let newString = nameName.trim( );
+  
+  console.log(newString.length);
+
+  // more methods are there check on mdn documentation
+
+  
+
+
