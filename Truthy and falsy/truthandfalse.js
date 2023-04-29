a602fc7584f3f660fbc15326dcba41592e0a99cb
Prakhar Singh <prakharsingh285@gmail.com>
2023-04-30 01:35:09 +0530

    updated

--- Truthy and falsy/truthandfalse.js
+++ Truthy and falsy/truthandfalse.js
@@ -1,18 +1,34 @@
 
 // truthy and falsy values;
 
-// false
+// false values
 //""
 //null
 //undefined
 //0
 
-let firstName = "";
+// truthy values
+//"abc"
+// 1 , -1
+
+
+let firstName = ""; // false
 
 if(firstName){
-    console.log(firstName);
+    console.log(firstName); 
+}else{
+    console.log("firstName is kinda empty"); // this block will run
+}
+ 
+
+let secondName = "prakhar"; //true
+
+if(firstName){
+    console.log(secondName); // this block will run
 }else{
     console.log("firstName is kinda empty");
 }
+
+
 
   
