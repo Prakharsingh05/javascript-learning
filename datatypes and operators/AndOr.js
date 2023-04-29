0098218ea0107bca081ff2c86e04528209f6079d
Prakhar Singh <prakharsingh285@gmail.com>
2023-04-30 01:50:24 +0530

    updated and skipped

---
+++ datatypes and operators/AndOr.js
@@ -0,0 +1,13 @@
+// and or operator
+
+let firstName = "prakhar";
+
+let age = 22;
+
+if(firstName[0] === "H"){
+    console.log("your name start with H");
+
+}
+
+// are ye sab bhot esy h so skippppppp moving to the next components
+// skipping sylabus to array skipped sylabus nested if else , is else if, switch statedment , while loop , for loop , break and continue  keyword ..  
