d31c075b4776ccf23032615ee3129dead83b3578
Prakhar Singh <prakharsingh285@gmail.com>
2023-05-01 18:54:47 +0530

    updated

---
+++ objects/objects.js
@@ -0,0 +1,24 @@
+//intro to objects
+//objects are refrence type same memory allocation like arrays
+//ojects dont have index
+
+
+//how to create objects
+
+const person = {
+
+    name: "parkar",
+    age: "20",
+    hobbies : ["guitar" , "singing"] // objects can contain array
+
+}
+
+console.log(person);
+
+console.log(person.name);
+
+console.log(person.hobbies);
+
+//hot to add key value pair to objects
+
+person.gender = "male";
