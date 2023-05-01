59043f6284d136006ea8655a7f14aa386107fbe8
Prakhar Singh <prakharsingh285@gmail.com>
2023-05-01 21:36:18 +0530

    updated

---
+++ objects/computedproperties.js
@@ -0,0 +1,17 @@
+//computed properties
+
+const key1 = "object1";
+const key2 = "object2";
+
+const value1 ="myvalue1";
+const value2 = "myvalue2";
+
+const obj = {
+
+    [key1] : value1,  //computed properties
+    [key2] : value2,  // computed properties
+
+}
+
+
+console.log(obj);
