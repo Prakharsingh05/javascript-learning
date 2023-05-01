5069a576e6529f2bf02055ef1fa3b3f1a83a40f0
Prakhar Singh <prakharsingh285@gmail.com>
2023-05-01 17:07:18 +0530

    updated

--- Arrays/array.js
+++ Arrays/array.js
@@ -26,13 +26,31 @@ console.log(fruit2);
 
 console.log(Array.isArray(fruit2)); // will return true
 
-//to add an element in an array use push
+//to add an element in an array from last use push
 
 fruit2.push("prakar"); // ads element in the last
 console.log(fruit2);
 fruit2.pop();// remove the last element from the last and it also return the last element
 console.log(fruit2);
+
+//to add an element in an array from starting use unshift
+
+
+fruit2.unshift("bananaabnabnbanabnabn");
+
+console.log(fruit2);
+fruit2.shift();// remove the last element from the start and it also return the start element
+
+console.log(fruit2);
+
+// push adn pop are faster than shift and unshit becoz of time complexity
+
+
+
+
+
 // objects
+
 
 
 
