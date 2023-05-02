09e2d3ccbbd44cba62218af3e343f29e56e7ebfd
Prakhar Singh <prakharsingh285@gmail.com>
2023-05-02 13:50:35 +0530

    updated

--- Scoping/blockScopeVsFuncScope.js
+++ Scoping/blockScopeVsFuncScope.js
@@ -1,4 +1,16 @@
 //Block scope vs Function Scope
 
 
-//
+// let and const ARE block scope
+// var is function scope
+
+{
+    let firstName = "Prakhar";
+}
+    console.log(firstName); // error firstName is not defined
+
+
+{
+   var secondName = "aishwarya";
+} 
+    console.log(secondName); //will print becoz can be accesed from anywhere
