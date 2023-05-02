3af9ffe04915c63554b62494bf2bea0daf189257
Prakhar Singh <prakharsingh285@gmail.com>
2023-05-02 14:40:30 +0530

    updated

---
+++ parameter/defaultParameter.js
@@ -0,0 +1,11 @@
+// default parameter
+
+
+function addTwo(a,b=0){ // if b value doesnt get passed here then it will tale 0 as assignend
+
+    return a+b;
+}
+
+
+const an = addTwo(4);
+console.log(ans);
