0ad522d187e08c95c8e748b546f257874f17e571
Prakhar Singh <prakharsingh285@gmail.com>
2023-05-03 21:35:17 +0530

    updated

---
+++ Array methods/filterMethod.js
@@ -0,0 +1,9 @@
+// filter method 
+const numbers = [1,4,55,6,7,8];
+
+const isEven(number){
+    return number%2===0;
+
+}
+
+const  evenNumber = number.filter(isEven);
--- Array methods/mapMethod.js
+++ Array methods/mapMethod.js
@@ -6,6 +6,6 @@ const numbers = [1,2,3,4,5];
 
 const square =function (number){
 
-    return number * number ;
+    return number * number ; // must return something if you want to use map method
 }
-const squareNumber = numbers.map(square);
+const squareNumber = numbers.map(square); 
