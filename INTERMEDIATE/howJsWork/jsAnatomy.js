626fd4782386744734258cc631a1abadfbd99348
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-02 15:37:18 +0530

    upated

--- INTERMEDIATE/howJsWork/jsAnatomy.js
+++ INTERMEDIATE/howJsWork/jsAnatomy.js
@@ -1,1 +1,21 @@
 // how js work 
+// compilation
+// code execution
+
+// How javascript code execites 
+
+// what is global exection context?
+// what is local execution context?
+// closure
+
+// compiling phase 
+// 1 tokeninzing / lexing // coe break into small chhunks
+// 2 parsing // abstratct stynstax tree 
+// 3 code generation ?/ excutable code formation gentratiion ehivh lawer
+
+console.log(this);
+console.log(window);
+console.log(furstName);
+var firstName = "Prakhar";
+
+
