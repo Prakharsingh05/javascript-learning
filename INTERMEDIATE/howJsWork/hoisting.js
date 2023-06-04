838dbb596326773d352d37d7a0634123a241f918
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-04 22:44:44 +0530

    uipdate

---
+++ INTERMEDIATE/howJsWork/hoisting.js
@@ -0,0 +1,7 @@
+//hoisting 
+ 
+ //printing or calling th e c=varialble b before  itd declaration hoisting can be happenn in   var type variable but not with oconst and let variable
+ 
+
+console.log(firstName);
+var firstName = "prakhar";
