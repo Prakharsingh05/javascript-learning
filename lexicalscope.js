488c99e6be8f6967e5539aeb521055f64559a769
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-05 14:15:20 +0530

    updated

--- lexicalscope.js
+++ lexicalscope.js
@@ -1,12 +1,7 @@
-//fucntion execution context 
+//lexical scoping
 
 
-// 1 - first step will  be global execution context
-//
 
-let foo = "foo";  // in memory creation phase the value of variable foo will be uninitalized becuase it is created using let
-
-console.log(foo);
 
 function getFullName(firstName , LastName){                                                                 //
     console.log(arguments); // array like objects not array it have a properties like indexing and length   //                  
@@ -14,12 +9,4 @@ function getFullName(firstName , LastName){                                     
     console.log(myVar);                                                                                     //        
     const fullName = firstName + " " + LastName;                                                            //    
     return fullName;                                                                                        //
-}     
-                                                                                                      //          
-// 1-function execution phase
-// 1.1 - code execution // 1.2 -local memory creation
-// when an variable is uninitialized then he stay in tdz (temporlal dead zone)
-// javascript moves from global excution context to function execution context
-
-const personName = getFullName("prakhar" , "singh");
-console.log(personName);
+} 
