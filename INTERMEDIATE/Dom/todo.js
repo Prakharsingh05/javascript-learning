4790b9e6c2f627b27131254b46eba70fdf4ff8da
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-30 23:18:13 +0530

    updayed

--- INTERMEDIATE/Dom/todo.js
+++ INTERMEDIATE/Dom/todo.js
@@ -34,13 +34,43 @@
 
 const mainHeading =document.querySelector("div.headline h2");
 const btn = document.querySelector(".change-clr");
-btn.onclick  = ()=>mainHeading.style.color = "green";
+btn.onclick  = ()=>{
+    mainHeading.style.color = (mainHeading.style.color=="green")? "white" :"green";
+}
 
 
 //get and set attributes
 
 const link = document.querySelector("a");
-console.log(link.getAnimations(href));
+console.log(link.getAttribute("href"));
+
+const inputElement = document.querySelector(".form-todo input");
+
+
+
+//get multiple elements using getElements by class name
+// get multiple elements items using querySelectorALL
+
+const navItems1 = document.getElementByClassName("nav-item"); // HTML collections 
+console.log(navItems[1]);// this is array like object not array{indexing}
+console.log(navItems[2]);
+console.log(navItems[3]);
+
+//simple for loop
+
+
+//for of loop
+
+
+
+//forEach - this cannot be used with HTML collection
+
+const navItems2 = document.querySelectorAll("nav-item"); // NodeList
+console.log(navItems[1]);// this is array like object not array {indexing} 
+console.log(navItems[2]);
+console.log(navItems[3]);
+
+
 
 
 
