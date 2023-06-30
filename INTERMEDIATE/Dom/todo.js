a75b7fb56123e61f4f3c8a821c63f409d9860d8b
Prakhar Singh <prakharsingh285@gmail.com>
2023-06-30 18:10:39 +0530

    dom

--- INTERMEDIATE/Dom/todo.js
+++ INTERMEDIATE/Dom/todo.js
@@ -1,3 +1,21 @@
 //dom
 
 //dcoument object moddel
+
+//select element using get element by i
+
+// const mainheading = document.getElementById("main-heading");
+
+//console.log(mainHeading);
+
+// select element using query selector
+
+// const mainHeading = document.querySelector("#main-heading");
+
+// console.log(mainHeading);
+
+
+
+
+
+ 
