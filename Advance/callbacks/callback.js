012a5d99df2bb5989942d87046cc46e6a111f998
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-02 15:43:18 +0530

    done

--- Advance/callbacks/callback.js
+++ Advance/callbacks/callback.js
@@ -1,1 +1,24 @@
 //understanding callbacks
+
+function myfunc(callback){
+    console.log("function is doing task 1");
+    callback();
+}
+
+function myfunc2(){
+
+    console.log("Function is doing task 2");
+
+}
+myfunc(myfunc2);
+
+function getTwoNumbersAndAdd(number1 , number2 , callback){
+    console.log(number1, number2);
+    callback(number1 , number2 v);
+}
+
+function addTwoNumbers(num1 , num2){
+    console.log(num1+num2);
+}
+
+getTwoNumbersAndAdd(4,5, addTwoNumbers);
