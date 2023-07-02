04018655f28266aab0fc157b3731f965ae8e02d3
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-02 14:08:25 +0530

    setINterval done

---
+++ Advance/101/index.js
@@ -0,0 +1,18 @@
+const body = document.body;
+const button = document.querySelector("button");
+
+const intervalId = setInterval(()=>{
+    const red =Math.floor(Math.random() * 126);
+    const green =Math.floor(Math.random() * 126);
+    const blue =Math.floor(Math.random() * 126);
+    const rgb=`rgb(${red},${green},${blue})`
+
+    body.style.background =rgb;
+},1000);
+
+
+button.addEventListener("click",()=>{
+    clearInterval(intervalId);
+    button.textContent =body.style.background;
+});
+
