c3464d3349d560a133e30e291ded7a9a033cbfb6
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-02 18:44:06 +0530

    updated

--- Advance/callbacks/callBackHell.js
+++ Advance/callbacks/callBackHell.js
@@ -5,16 +5,16 @@
 const heading1 = document.querySelector(".heading1");
 const heading2 = document.querySelector(".heading2");
 
-// setTimeout(()=>{
-//     heading1.textContent="Heading 1";
-//     heading1.style.color ="violet";
-//     setTimeout(()=>{
-//         heading2.style.color ="violet";
-//         heading2.textContent="Heading 2";
+setTimeout(()=>{
+    heading1.textContent="Heading 1";
+    heading1.style.color ="violet";
+    setTimeout(()=>{
+        heading2.style.color ="violet";
+        heading2.textContent="Heading 2";
 
-//     },1000);
+    },1000);
 
-// },1000);
+},1000);
 
 
 function ChangeText(element , text , color, time , onSuccessCallback , onFailureCallback ){
