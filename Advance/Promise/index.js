0c58bf51683fbf0bff68e6338a2da188de438e6a
Prakhar Singh <prakharsingh285@gmail.com>
2023-07-15 22:15:32 +0530

    promise updated

--- Advance/Promise/index.js
+++ Advance/Promise/index.js
@@ -2,3 +2,26 @@
 
 const bucket = ['coffee' , 'chips' , 'vegetable' , 'salt' , 'rice' ];
 
+// promise is type of value which we dont know now but we will be knowing that value in the future
+
+
+
+//creating promise
+const friedRicePromise=new Promise((resolve , reject)=>{
+            if(bucket.include("vegetables")&& bucket.includes("salt")&& bucket.includes("salt")){
+                resolve("Fried Rice");
+
+            }else{
+                reject(Error('not found'));
+            }    
+});
+
+friedRicePromise.then(
+    //this will run if promise will resolve 
+(datafrompromise)=>{
+
+    console.log("lets eat",datafrompromise);
+},
+
+//this will run if proise will reject
+(error)=>{console.log(error)})
